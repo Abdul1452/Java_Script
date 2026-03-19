f2457e1cf38b7e245c71fb294552a86f94799c5c
ABDULLAHI OLAMILEKAN ABDULWASIU <abdullahi.abdulwasiu@edu.turkuamk.fi>
2026-03-19 18:19:40 +0200

    Chapter 1: Learning basics in JavaScript

---
+++ Chapter_1/sandbox.js
@@ -0,0 +1,14 @@
+let age = 25;
+let year = 2019;
+
+// log things to console
+console.log(age); 
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50;
+console.log(points);
+
+var score = 75;
+console.log(score);
---
+++ Chapter_1/strings.js
@@ -0,0 +1,20 @@
+// strings
+comsole.log("hello, world");
+
+let email = "ola@learningjavascript.fi"
+console.log(email)
+
+
+// string concatenation
+let firstName = "Ola"
+let lastName = "Ayina"
+
+let fullName = firstName + "" + lastName
+
+console.log(fullName);
+
+// getting characters 
+console.log(fullName[0])
+
+// string length 
+
