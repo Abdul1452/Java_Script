52738278d83f949d08660fe225fa4a2916646e65
ABDULLAHI OLAMILEKAN ABDULWASIU <abdullahi.abdulwasiu@edu.turkuamk.fi>
2026-03-19 19:56:46 +0200

    did some number operations

---
+++ Chapter_1/numbers.js
@@ -0,0 +1,35 @@
+let radius = 10;
+const pi = 3.14; 
+
+// console.log(radius, pi)
+
+// math operators +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+// order of opeartion - B I D M A S
+// let result = 5 * (10-3)**2;
+
+let likes = 10;
+// Shorthand Notations 
+// likes = likes + 1;
+// likes++;
+// likes--;
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+console.log(likes);
+// console.log(result) 
+
+// Nan - not a number
+
+console.log(5 / "hello")
+
+//  Number conacatenation
+
+let outcome = "The blog has" + likes + "likes";
+console.log(outcome);
